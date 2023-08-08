c495998000b8e4ef86874d1230ca6d346846f5c5
RACHID-WEBDEV <rasheedolamide2015@gmail.com>
2023-08-08 13:46:12 +0100

    Fix broken images3

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 
-const repo = "hubit";
-const assetPrefix = `/${repo}/`;
-const basePath = `/${repo}`;
+// const repo = "hubit";
+// const assetPrefix = `/${repo}/`;
+// const basePath = `/${repo}`;
 
 const nextConfig = {
   trailingSlash: true,
   output: "export",
-  basePath: basePath,
-  assetPrefix: assetPrefix,
+  // basePath: basePath,
+  // assetPrefix: assetPrefix,
   images: {
     unoptimized: true,
   },
