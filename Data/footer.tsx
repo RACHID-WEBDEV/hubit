492c66a9ed561b9637d0fd323c7c97dec0f8438f
RACHID-WEBDEV <rasheedolamide2015@gmail.com>
2023-08-12 17:27:06 +0100

    Create New Components

---
+++ Data/footer.tsx
@@ -0,0 +1,97 @@
+export const footer = [
+  {
+    title: "Navigation",
+    links: [
+      {
+        name: "About",
+        link: "/about",
+        leavesWebsite: false,
+      },
+      {
+        name: "Services",
+        link: "/services",
+        leavesWebsite: false,
+      },
+      {
+        name: "Work",
+        link: "/work",
+        leavesWebsite: false,
+      },
+      {
+        name: "Blogs",
+        link: "/blog",
+        leavesWebsite: false,
+      },
+
+      {
+        name: "Contact",
+        link: "/contact",
+        leavesWebsite: false,
+      },
+    ],
+    hideMobile: false,
+  },
+  {
+    title: "Resources",
+    links: [
+      {
+        name: "Faqs",
+        link: "/",
+        leavesWebsite: false,
+      },
+      {
+        name: "Privacy Policy",
+        link: "/",
+        leavesWebsite: false,
+      },
+      {
+        name: "Terms Conditions",
+        link: "/",
+        leavesWebsite: false,
+      },
+      {
+        name: "Careers",
+        link: "/",
+        leavesWebsite: false,
+      },
+      {
+        name: "Internship",
+        link: "/",
+        leavesWebsite: false,
+      },
+    ],
+    hideMobile: false,
+  },
+  {
+    title: "Socials",
+    links: [
+      {
+        name: "Twitter",
+        link: "https://www.twitter.com",
+        leavesWebsite: true,
+      },
+      {
+        name: "Facbook",
+        link: "https://www.facbook.com",
+        leavesWebsite: true,
+      },
+      {
+        name: "Instagram",
+        link: "https://www.instagram.com",
+        leavesWebsite: true,
+      },
+      {
+        name: "GitHub",
+        link: "https://github.com/",
+        leavesWebsite: true,
+      },
+
+      {
+        name: "LinkedIn",
+        link: "https://linkedIn.com",
+        leavesWebsite: true,
+      },
+    ],
+    hideMobile: true,
+  },
+];
