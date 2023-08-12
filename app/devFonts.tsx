861d1c9b3aeeed1d1c4a1c4b6d2abbd302fa0477
RACHID-WEBDEV <rasheedolamide2015@gmail.com>
2023-08-12 17:26:03 +0100

    Create New Components

---
+++ app/devFonts.tsx
@@ -0,0 +1,60 @@
+import localFont from "next/font/local";
+
+const inter = localFont({
+  src: [
+    {
+      path: "../public/fonts/Inter/Inter-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Inter/Inter-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Inter/Inter-SemiBold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Inter/Inter-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Inter/Inter-ExtraBold.ttf",
+      weight: "800",
+      style: "normal",
+    },
+  ],
+  variable: "--font-Inter",
+});
+
+const syne = localFont({
+  src: [
+    {
+      path: "../public/fonts/Syne/Syne-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Syne/Syne-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Syne/Syne-Semibold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Syne/Syne-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-Syne",
+});
+
+export { inter, syne };
