684e1c301b484fb7f21f9401f90fe83745b424c8
RACHID-WEBDEV <rasheedolamide2015@gmail.com>
2023-08-12 17:27:02 +0100

    Create New Components

---
+++ Data/brandsData.tsx
@@ -0,0 +1,7 @@
+export const brandsData = [
+  "/images/brands/Airbnb.svg",
+  "/images/brands/Pie Insurance.svg",
+  "/images/brands/Mosaic.svg",
+  "/images/brands/Vestwell.svg",
+  "/images/brands/Uber.svg",
+];
