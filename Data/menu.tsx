9badd3ec74bc6ffbcd70bb4b5039ceb6434f473c
RACHID-WEBDEV <rasheedolamide2015@gmail.com>
2023-08-12 17:27:10 +0100

    Create New Components

---
+++ Data/menu.tsx
@@ -0,0 +1,28 @@
+export type menusType = {
+  title: string;
+  link: string;
+};
+const menus: menusType[] = [
+  {
+    title: "About",
+    link: "/about",
+  },
+  {
+    title: "Solution",
+    link: "/solution",
+  },
+  {
+    title: "Pricing",
+    link: "/pricing",
+  },
+  {
+    title: "Blog",
+    link: "/blog",
+  },
+  {
+    title: "Contact",
+    link: "/contact",
+  },
+];
+
+export default menus;
