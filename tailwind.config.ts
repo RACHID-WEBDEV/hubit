a9587829b9a1512c706da8c3ff1d0e73d304a300
RACHID-WEBDEV <rasheedolamide2015@gmail.com>
2023-08-12 17:25:58 +0100

    Create New Components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,29 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: "#EDA735",
+        primaryAlt: "#FAD783",
+        backgroundColor: "#F0EFEA",
+        backgroundAlt: "#E0DCD3",
+        secondary: "#1C1A1F",
+      },
+      backgroundImage: {},
+      fontFamily: {
+        // Syne: ["Syne", "sans-serif"],
+        // Inter: ["Inter", "sans-serif"],
+        Inter: ["var(--font-Inter)", "sans-serif"],
+        Syne: ["var(--font-Syne)", "sans-serif"],
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
